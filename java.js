f9f1e1dcb2d182ec53bb1e1c2221b5eae1aad175
Adhambek0202 <adhambekismatov0212@gmail.com>
2022-05-17 17:56:05 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,9 @@
+alert ("bu sonni toq yoki juft ekanini aniqlaydigan sayt")
+let son = prompt ("birorta son kiriting")
+let son2 = son%2
+if (son2==0) {
+    alert ("bu son juft")
+}else {
+    alert ("bu son toq")
+}
+
---
+++ main.js
@@ -0,0 +1,13 @@
+let yil = 2022
+alert ("Hello !")
+
+let ism = prompt ("Iltimos ismingiznini kiriting")
+let familya = prompt ("Iltimos familyangizni kiriting")
+let yoshingiz = prompt ("tug'ilgan sanagizni kiriting")
+alert (ism +' ' + familya + ' '+ yoshingiz )
+let yosh = 2003
+if (yoshingiz >= yosh){
+    alert('Hali maktabda uqib tur..!');
+} else if (yoshingiz <  yosh) {
+    alert ('Uji armiyadasan..!');
+}
